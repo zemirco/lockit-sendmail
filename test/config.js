e15580eecfb7b6ba5cdaac713e58db918b455131
zeMirco <mirco.zeiss@gmail.com>
2013-12-17 20:56:55 +0100

    removed old config data

--- test/config.js
+++ test/config.js
@@ -1,9 +1,5 @@
 exports.appname = 'Test App';
 exports.url = 'http://localhost:3000';
-exports.port = 3000; // Todo: use port instead full url
-
-// show debugging statements in console
-exports.debug = false;
 
 // signup settings
 exports.signupRoute = '/signup';
